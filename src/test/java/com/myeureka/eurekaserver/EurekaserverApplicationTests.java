cd41ebb0384256ef1c240b0cdb0dc471a09310d2
Sangita Kumari Rath <sangita.r9@gmail.com>
2020-07-04 14:29:26 +0530

    Initial commit

---
+++ src/test/java/com/myeureka/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myeureka.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
